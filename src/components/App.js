8023d8836a29bdd06da9842057314e6e3819c4b9
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-09-03 22:38:27 +0200

    add style timer

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Countdown from "./Countdown";
 
 const App = () => {
   return (
     <div className="app">
       <div className="page">
         <Header />
-        <div className="timer__container">
-          <div className="timer">
-            <i class="far fa-clock"></i>
-            <h2>Finalización de la oferta</h2>
-          </div>
-        </div>
+        <Countdown />
         <Footer />
       </div>
     </div>
--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,5 +1,28 @@
+import "../stylesheet/components/countdown.scss";
+
 const Countdown = () => {
-  return <div></div>;
+  return (
+    <div className="timer__container">
+      <div className="timer">
+        <i class="timer__icon far fa-clock"></i>
+        <h2 className="timer_title">Finalización de la oferta</h2>
+        <div className="timer__info">
+          <p>87</p>
+          <p>
+            <small>Días</small>
+          </p>
+          <p>87</p>
+          <p>
+            <small>Horas</small>
+          </p>
+          <p>87</p>
+          <p>
+            <small>Minutos</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Countdown;
